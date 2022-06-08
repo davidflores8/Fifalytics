f9fddd96ad0b295a1b1bf4adf737c5ff77bdf9dc
brauliocalix55 <djbrzhn1@hotmail.com>
2022-06-08 10:57:34 -0600

    agarra imagenes

--- Menu.py
+++ Menu.py
@@ -1,17 +1,33 @@
+from cgi import print_arguments
 from cgitb import text
+from turtle import bgcolor
 import cv2
-from cv2 import CALIB_HAND_EYE_ANDREFF
+import os
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Prestamo\AppData\Local\Programs\Tesseract-OCR\tesseract"
+#ruta="./Fotos Proyecto Inteligentes/"
+clahe=cv2.createCLAHE(clipLimit=0.5,tileGridSize=(20,20))
 
-image= cv2.imread("./Fotos Proyecto Inteligentes/verrati.png")
-texto=pytesseract.image_to_string(image, lang=None)
-cadena=""
-for i in texto:
-    if i.isdigit():
-        cadena+=str(i)
-print (cadena)
-print (texto)
-cv2.imshow("image: ", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+f = open("./imagenes.txt", "r")
+for i in f:
+    ruta=""
+    cadena=str(i)
+    cadena=cadena.replace("\n","")
+    ruta="./Fotos Proyecto Inteligentes/"+cadena
+    print(ruta)
+    image= cv2.imread(ruta)
+    imageC=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    imageE=cv2.equalizeHist(imageC)
+    imageD=clahe.apply(imageE)
+    texto=pytesseract.image_to_string(imageD)
+    print (texto)    
+    cv2.imshow("image: ", imageD)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+f.close
+
+
+
+#cv2.imshow("image: ", imageD)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
