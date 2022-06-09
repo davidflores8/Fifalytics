869bbf233bc291856000c24a90bd04afcbb1cc18
brauliocalix55 <djbrzhn1@hotmail.com>
2022-06-08 21:03:11 -0600

    blanco y negro

--- Menu.py
+++ Menu.py
@@ -1,33 +1,34 @@
 from cgi import print_arguments
 from cgitb import text
+from tkinter import image_names
 from turtle import bgcolor
 import cv2
 import os
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Prestamo\AppData\Local\Programs\Tesseract-OCR\tesseract"
 #ruta="./Fotos Proyecto Inteligentes/"
-clahe=cv2.createCLAHE(clipLimit=0.5,tileGridSize=(20,20))
+clahe=cv2.createCLAHE(clipLimit=3.5,tileGridSize=(10,10))
 
 f = open("./imagenes.txt", "r")
+print(pytesseract.get_languages(config=''))
 for i in f:
     ruta=""
     cadena=str(i)
     cadena=cadena.replace("\n","")
     ruta="./Fotos Proyecto Inteligentes/"+cadena
-    print(ruta)
+    print(ruta)      
     image= cv2.imread(ruta)
     imageC=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    imageE=cv2.equalizeHist(imageC)
-    imageD=clahe.apply(imageE)
-    texto=pytesseract.image_to_string(imageD)
+    (thresh,negro)=cv2.threshold(imageC,127,255,cv2.THRESH_BINARY)
+    #imageE=cv2.equalizeHist(negro)
+    #imageD=clahe.apply(imageE)
+    
+    texto=pytesseract.image_to_string(negro,lang="eng")
     print (texto)    
-    cv2.imshow("image: ", imageD)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()    
+    #cv2.imshow("image: ", negro) 
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()    
 f.close
-
-
-
 #cv2.imshow("image: ", imageD)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
